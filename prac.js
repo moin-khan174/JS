37477231452fde5a8e66dca5095cd1a5af66eaa4
moin-khan174 <moink7511@gmail.com>
2025-04-16 11:53:55 +0530

    Added prac.js

---
+++ prac.js
@@ -0,0 +1,50 @@
+// (function(x){
+//     return 	(function(y){
+//           console.log(x)
+//     })(1)
+//     })(2);
+    
+
+// const obj = {
+// name: "John",
+// greet: function() {
+// setTimeout(function() {
+// console.log(this.name);
+// }, 1000);
+// }
+// };
+// obj.greet();
+
+
+// const a = { x: 10 };
+
+// const b = a;
+// b.x = 20;
+// console.log(a.x);
+    
+// (function(){
+//     var a=b=3;
+// })()
+//     console.log("a defined"+(typeof a!=='undefined'))
+//     console.log("a defined"+(typeof b!=='undefined'))
+
+    // console.log(0.1+0.2)
+    // console.log((0.1+0.2==0.3))
+
+    // (function(){
+    //     console.log(1)
+    //     setTimeout(function(){
+    //         console.log(2)
+    //     },1000)
+    //     setTimeout(function(){
+    //         console.log(3)
+    //     },0)
+    //     console.log(4)
+    // })();
+
+    console.log(1+"1"+"2")
+    console.log((1+ +"2"+"2"))
+    console.log((1+ -"2"+"2"))
+    console.log(+"1"+"1"+"2")
+    console.log("A"-"B"+"2")
+    console.log("A"-"B"+2)
